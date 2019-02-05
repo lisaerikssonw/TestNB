18511789cc8eda8c8c893047a34567451ebc2ba1
Lisa E <lisa.eriksson.w@gmail.com>
2019-02-05 10:14:50 +0100

    Initial commit

---
+++ src/sortit/SortDescending.java
@@ -0,0 +1,12 @@
+package sortit;
+
+import java.util.Comparator;
+
+public class SortDescending implements Comparator<Integer> {
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o2.compareTo(o1);
+    }
+
+}
